d1d46f1a729d283b188429500907f8db0b8d1141
yogajulianp <yogajprasutiyo@gmail.com>
2023-01-12 19:35:32 +0700

    update model

--- POS/POS.ViewModel/CustomerModel.cs
+++ POS/POS.ViewModel/CustomerModel.cs
@@ -1,60 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace POS.Repository
+namespace POS.ViewModel
 {
-    [Table("tbl_customers")]
-    public class CustomersEntity
+    public class CustomerModel
     {
-        [Key]
-        [Column("id")]
         public int Id { get; set; }
 
         [Required]
-        [Column("company_name")]
-        public String CompanyName{ get; set; }
+        public String CompanyName { get; set; }
 
         [Required]
-        [Column("contact_name")]
         public String ContactName { get; set; }
 
         [Required]
-        [Column("contact_title")]
         public String ContactTitle { get; set; }
 
         [Required]
-        [Column("address")]
         public String Address { get; set; }
 
         [Required]
-        [Column("city")]
         public String City { get; set; }
 
         [Required]
-        [Column("region")]
         public String Region { get; set; }
 
         [Required]
-        [Column("postal_code")]
         public String PostalCode { get; set; }
 
         [Required]
-        [Column("country")]
         public String Country { get; set; }
 
         [Required]
-        [Column("phone")]
         public String Phone { get; set; }
 
         [Required]
-        [Column("fax")]
         public String Fax { get; set; }
 
-        public ICollection<OrdersEntity> Orders { get; set; }
     }
 }
